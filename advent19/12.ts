250f9823f9e3a661edf8ba86a9a55c876ecddd86
Tom Saunders <tom@tsaunders.net>
2019-12-16 00:14:18 +1100

    day 14/15

--- advent19/12.ts
+++ advent19/12.ts
@@ -6,13 +6,8 @@ class Moon {
   public vx: number = 0;
   public vy: number = 0;
   public vz: number = 0;
-  public seen: Map<string, number>;
-  public loopSize: number = 0;
 
-  constructor(public x: number, public y: number, public z: number) {
-    this.seen = new Map<string, number>();
-    this.seen.set(this.status, 0);
-  }
+  constructor(public x: number, public y: number, public z: number) {}
 
   public get status(): string {
     return `pos= ${this.x}, ${this.y}, ${this.z} vel= ${this.vx}, ${this.vy}, ${this.vz}`;
@@ -58,52 +53,18 @@ class Moon {
     }
   }
 
-  public move(time: number): void {
+  public move(): void {
     this.x += this.vx;
     this.y += this.vy;
     this.z += this.vz;
-    if (this.loopSize) {
-      // return;
-    }
-    // const status = this.status;
-    // if (this.seen.has(status)) {
-    //   const loop = time - (this.seen.get(status) as number);
-    //   if (!this.loopSize || loop < this.loopSize) {
-    //     this.loopSize = loop;
-    //     console.log(
-    //       time,
-    //       `${status} seen previously at `,
-    //       this.seen.get(status),
-    //       "loop size is",
-    //       loop
-    //     );
-    //   }
-    // } else {
-    //   this.seen.set(status, time);
-    // }
   }
 }
 
-const testMoons = [
-  new Moon(-1, 0, 2),
-  new Moon(2, -10, -7),
-  new Moon(4, -8, 8),
-  new Moon(3, 5, -1)
-];
+const testMoons = [new Moon(-1, 0, 2), new Moon(2, -10, -7), new Moon(4, -8, 8), new Moon(3, 5, -1)];
 
-const test2 = [
-  new Moon(-8, -10, 0),
-  new Moon(5, 5, 10),
-  new Moon(2, -7, 3),
-  new Moon(9, -8, -3)
-];
+const test2 = [new Moon(-8, -10, 0), new Moon(5, 5, 10), new Moon(2, -7, 3), new Moon(9, -8, -3)];
 
-const inputMoons = [
-  new Moon(1, 4, 4),
-  new Moon(-4, -1, 19),
-  new Moon(-15, -14, 12),
-  new Moon(-17, 1, 10)
-];
+const inputMoons = [new Moon(1, 4, 4), new Moon(-4, -1, 19), new Moon(-15, -14, 12), new Moon(-17, 1, 10)];
 
 function run(moons: Moon[], time: number): void {
   for (let m = 0; m < moons.length; m++) {
@@ -111,7 +72,7 @@ function run(moons: Moon[], time: number): void {
     moon.gravity(moons.slice(m + 1));
   }
   for (const moon of moons) {
-    moon.move(time);
+    moon.move();
   }
 }
 
@@ -141,13 +102,13 @@ console.log("\nPart 2");
 
 function steps(moons: Moon[]): number {
   const seen = new Map<string, number>();
-  let hash = moons.map(m => m.status).join("::");
+  let hash = moons.map((m) => m.status).join("::");
   let t = 0;
   while (!seen.has(hash)) {
     seen.set(hash, t);
     run(moons, t);
     t++;
-    hash = moons.map(m => m.status).join("::");
+    hash = moons.map((m) => m.status).join("::");
     if (seen.has(hash)) {
       return t;
     }
@@ -159,9 +120,9 @@ function axisSeen(moons: Moon[]): number {
   const xMap = new Map<string, number>();
   const yMap = new Map<string, number>();
   const zMap = new Map<string, number>();
-  let xhash = moons.map(m => `${m.x}:${m.vx}`).join("::");
-  let yhash = moons.map(m => `${m.y}:${m.vy}`).join("::");
-  let zhash = moons.map(m => `${m.z}:${m.vz}`).join("::");
+  let xhash = moons.map((m) => `${m.x}:${m.vx}`).join("::");
+  let yhash = moons.map((m) => `${m.y}:${m.vy}`).join("::");
+  let zhash = moons.map((m) => `${m.z}:${m.vz}`).join("::");
   xMap.set(xhash, 0);
   yMap.set(yhash, 0);
   zMap.set(zhash, 0);
@@ -173,9 +134,9 @@ function axisSeen(moons: Moon[]): number {
   while (!xLoop || !yLoop || !zLoop) {
     run(moons, t);
     t++;
-    xhash = moons.map(m => `${m.x}:${m.vx}`).join("::");
-    yhash = moons.map(m => `${m.y}:${m.vy}`).join("::");
-    zhash = moons.map(m => `${m.z}:${m.vz}`).join("::");
+    xhash = moons.map((m) => `${m.x}:${m.vx}`).join("::");
+    yhash = moons.map((m) => `${m.y}:${m.vy}`).join("::");
+    zhash = moons.map((m) => `${m.z}:${m.vz}`).join("::");
 
     if (!xLoop) {
       if (xMap.has(xhash)) {
@@ -205,16 +166,41 @@ function axisSeen(moons: Moon[]): number {
   let stepTry = mul * max;
 
   console.log("Finding first common multiples of ", xLoop, yLoop, zLoop);
-  while (
-    stepTry % xLoop !== 0 ||
-    stepTry % yLoop !== 0 ||
-    stepTry % zLoop !== 0
-  ) {
-    mul++;
-    stepTry = mul * max;
+
+  function gcd(a: number, b: number) {
+    // Euclidean algorithm
+    var t;
+    while (b != 0) {
+      t = b;
+      b = a % b;
+      a = t;
+    }
+    return a;
   }
 
-  return stepTry;
+  function lcm(a: number, b: number) {
+    return (a * b) / gcd(a, b);
+  }
+
+  function lcmm(args: number[]): number {
+    // Recursively iterate through pairs of arguments
+    // i.e. lcm(args[0], lcm(args[1], lcm(args[2], args[3])))
+
+    if (args.length == 2) {
+      return lcm(args[0], args[1]);
+    } else {
+      var arg0 = args[0];
+      args.shift();
+      return lcm(arg0, lcmm(args));
+    }
+  }
+
+  // while (stepTry % xLoop !== 0 || stepTry % yLoop !== 0 || stepTry % zLoop !== 0) {
+  //   mul++;
+  //   stepTry = mul * max;
+  // }
+
+  return lcmm([xLoop, yLoop, zLoop]);
 }
 
 test(2772, steps(testMoons));
---
+++ advent19/14.ts
@@ -0,0 +1,229 @@
+#!/usr/bin/env npx ts-node
+import * as fs from "fs";
+const input = fs.readFileSync("input14.txt", "utf8") as string;
+
+function test(a: any, b: any): void {
+  const o = a == b ? `Test pass = ${a}` : `!!Test fail got ${b} wanted ${a}`;
+  console.log(o);
+}
+
+class Chemical {
+  public qty: number;
+  public name: string;
+  constructor(input: string) {
+    const bits = input.split(" ");
+    this.qty = parseInt(bits[0]);
+    this.name = bits[1];
+  }
+}
+
+class Formula {
+  public name: string;
+  public qty: number;
+  public source: Map<string, number>;
+
+  constructor(input: string) {
+    const bits = input.split(" => ");
+    this.source = new Map<string, number>();
+    bits[0]
+      .split(", ")
+      .map((c) => new Chemical(c))
+      .forEach((c) => this.source.set(c.name, c.qty));
+    const result = new Chemical(bits[1]);
+    this.name = result.name;
+    this.qty = result.qty;
+  }
+}
+
+function fuel(input: string): number {
+  const lines = input.split("\n");
+  const formulae = lines.map((l) => new Formula(l));
+  const lookup = new Map<string, Formula>();
+  const surplus = new Map<string, number>();
+  formulae.forEach((f) => {
+    surplus.set(f.name, 0);
+    lookup.set(f.name, f);
+  });
+
+  const fuelResult = lookup.get("FUEL") as Formula;
+  let fSource = fuelResult.source;
+
+  let x = 0;
+  while (fSource.size !== 1 || !fSource.has("ORE")) {
+    const chemicals = Array.from(fSource.keys());
+    for (const chem of chemicals) {
+      if (chem !== "ORE") {
+        const chemForm = lookup.get(chem) as Formula;
+        let fetch = fSource.get(chem) as number;
+        let inSurplus = surplus.get(chem) as number;
+
+        if (inSurplus >= fetch) {
+          fetch = 0;
+          inSurplus -= fetch;
+        } else {
+          fetch -= inSurplus;
+
+          const mul = Math.ceil(fetch / chemForm.qty);
+          const chemMade = mul * chemForm.qty;
+
+          inSurplus = chemMade - fetch;
+          // console.log(`Made ${chemMade} seeking ${fetch} of ${chem} - surplus now ${inSurplus}`);
+
+          for (const key of chemForm.source.keys()) {
+            const chemQty = chemForm.source.get(key) as number;
+            const curr = (fSource.get(key) as number) || 0;
+            fSource.set(key, curr + chemQty * mul);
+            // console.log(`${key} now at `, curr + chemQty * mul);
+          }
+        }
+        surplus.set(chem, inSurplus);
+        fSource.delete(chem);
+      }
+    }
+  }
+  return fSource.get("ORE") as number;
+}
+
+const test1a = `10 ORE => 10 A
+1 ORE => 1 B
+7 A, 1 B => 1 C
+7 A, 1 C => 1 D
+7 A, 1 D => 1 E
+7 A, 1 E => 1 FUEL`;
+
+const test1b = `9 ORE => 2 A
+8 ORE => 3 B
+7 ORE => 5 C
+3 A, 4 B => 1 AB
+5 B, 7 C => 1 BC
+4 C, 1 A => 1 CA
+2 AB, 3 BC, 4 CA => 1 FUEL`;
+
+const test1c = `157 ORE => 5 NZVS
+165 ORE => 6 DCFZ
+44 XJWVT, 5 KHKGT, 1 QDVJ, 29 NZVS, 9 GPVTF, 48 HKGWZ => 1 FUEL
+12 HKGWZ, 1 GPVTF, 8 PSHF => 9 QDVJ
+179 ORE => 7 PSHF
+177 ORE => 5 HKGWZ
+7 DCFZ, 7 PSHF => 2 XJWVT
+165 ORE => 2 GPVTF
+3 DCFZ, 7 NZVS, 5 HKGWZ, 10 PSHF => 8 KHKGT`;
+
+const test1d = `2 VPVL, 7 FWMGM, 2 CXFTF, 11 MNCFX => 1 STKFG
+17 NVRVD, 3 JNWZP => 8 VPVL
+53 STKFG, 6 MNCFX, 46 VJHF, 81 HVMC, 68 CXFTF, 25 GNMV => 1 FUEL
+22 VJHF, 37 MNCFX => 5 FWMGM
+139 ORE => 4 NVRVD
+144 ORE => 7 JNWZP
+5 MNCFX, 7 RFSQX, 2 FWMGM, 2 VPVL, 19 CXFTF => 3 HVMC
+5 VJHF, 7 MNCFX, 9 VPVL, 37 CXFTF => 6 GNMV
+145 ORE => 6 MNCFX
+1 NVRVD => 8 CXFTF
+1 VJHF, 6 MNCFX => 4 RFSQX
+176 ORE => 6 VJHF`;
+
+const test1e = `171 ORE => 8 CNZTR
+7 ZLQW, 3 BMBT, 9 XCVML, 26 XMNCP, 1 WPTQ, 2 MZWV, 1 RJRHP => 4 PLWSL
+114 ORE => 4 BHXH
+14 VRPVC => 6 BMBT
+6 BHXH, 18 KTJDG, 12 WPTQ, 7 PLWSL, 31 FHTLT, 37 ZDVW => 1 FUEL
+6 WPTQ, 2 BMBT, 8 ZLQW, 18 KTJDG, 1 XMNCP, 6 MZWV, 1 RJRHP => 6 FHTLT
+15 XDBXC, 2 LTCX, 1 VRPVC => 6 ZLQW
+13 WPTQ, 10 LTCX, 3 RJRHP, 14 XMNCP, 2 MZWV, 1 ZLQW => 1 ZDVW
+5 BMBT => 4 WPTQ
+189 ORE => 9 KTJDG
+1 MZWV, 17 XDBXC, 3 XCVML => 2 XMNCP
+12 VRPVC, 27 CNZTR => 2 XDBXC
+15 KTJDG, 12 BHXH => 5 XCVML
+3 BHXH, 2 VRPVC => 7 MZWV
+121 ORE => 7 VRPVC
+7 XCVML => 6 RJRHP
+5 BHXH, 4 VRPVC => 5 LTCX`;
+
+test(31, fuel(test1a));
+test(165, fuel(test1b));
+test(13312, fuel(test1c));
+test(180697, fuel(test1d));
+test(2210736, fuel(test1e));
+console.log("Part 1 answer:", fuel(input));
+
+// part 2
+console.log("\nPart 2");
+function trillionaire(input: string): number {
+  const trillion = 1000000000000;
+  const lines = input.split("\n");
+  const formulae = lines.map((l) => new Formula(l));
+  const lookup = new Map<string, Formula>();
+  const surplusMaster = new Map<string, number>();
+  formulae.forEach((f) => {
+    surplusMaster.set(f.name, 0);
+    lookup.set(f.name, f);
+  });
+
+  const fuelResult = lookup.get("FUEL") as Formula;
+
+  const singleCost = fuel(input);
+
+  let lower = Math.ceil(trillion / singleCost);
+  let upper = lower * 2;
+  // console.log("single cost", singleCost, "trying", lower, "and", upper);
+
+  while (lower !== upper) {
+    const mul = Math.round(lower + (upper - lower) / 2);
+    // console.log("Trying", mul);
+
+    let fSource = new Map(fuelResult.source);
+    const surplus = new Map(surplusMaster);
+    for (const key of fSource.keys()) {
+      const chemQty = fSource.get(key) as number;
+      const curr = (fSource.get(key) as number) || 0;
+      fSource.set(key, curr + chemQty * mul);
+      // console.log(`${key} now at `, curr + chemQty * mul);
+    }
+    while (fSource.size !== 1 || !fSource.has("ORE")) {
+      const chemicals = Array.from(fSource.keys());
+      for (const chem of chemicals) {
+        if (chem !== "ORE") {
+          const chemForm = lookup.get(chem) as Formula;
+          let fetch = fSource.get(chem) as number;
+          let inSurplus = surplus.get(chem) as number;
+
+          if (inSurplus >= fetch) {
+            fetch = 0;
+            inSurplus -= fetch;
+          } else {
+            fetch -= inSurplus;
+
+            const mul = Math.ceil(fetch / chemForm.qty);
+            const chemMade = mul * chemForm.qty;
+
+            inSurplus = chemMade - fetch;
+            // console.log(`Made ${chemMade} seeking ${fetch} of ${chem} - surplus now ${inSurplus}`);
+
+            for (const key of chemForm.source.keys()) {
+              const chemQty = chemForm.source.get(key) as number;
+              const curr = (fSource.get(key) as number) || 0;
+              fSource.set(key, curr + chemQty * mul);
+              // console.log(`${key} now at `, curr + chemQty * mul);
+            }
+          }
+          surplus.set(chem, inSurplus);
+          fSource.delete(chem);
+        }
+      }
+    }
+    const ore = fSource.get("ORE") as number;
+    const ratio = trillion / ore;
+    if (ore < trillion) {
+      lower = mul;
+    } else {
+      upper = mul - 1;
+    }
+  }
+  return lower + 1;
+}
+
+test(82892753, trillionaire(test1c));
+test(5586022, trillionaire(test1d));
+test(460664, trillionaire(test1e));
+console.log("Answer:", trillionaire(input));
