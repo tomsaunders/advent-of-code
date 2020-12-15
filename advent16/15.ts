5d0ff5c73b2acdb5a4cc8fea7b830bc415e21902
Tom Saunders <tom.saunders@smartward.com.au>
2020-12-15 14:18:56 +1100

    2016.17

--- advent16/15.ts
+++ advent16/15.ts
@@ -10,52 +10,46 @@ const input = fs.readFileSync("input15.txt", "utf8");
 // Disc #5 has 3 positions; at time=0, it is at position 0.
 // Disc #6 has 13 positions; at time=0, it is at position 5.
 
-// t0 c0
-// t1 c1
-// t2 c2
-// t3 c0
-// t4 c1
-// t5 c2
+type Discs = [number, number][];
+const inputDiscs: Discs = [
+  [17, 1],
+  [7, 0],
+  [19, 2],
+  [5, 0],
+  [3, 0],
+  [13, 5],
+];
 
-const discs: [number, number][] = [
-  // [17, 1],
-  // [7, 0],
-  // [19, 2],
-  // [5, 0],
-  // [3, 0],
-  // [13, 5],
+const testDiscs: Discs = [
   [5, 4],
   [2, 1],
 ];
 
-for (let d = 0; d < discs.length; d++) {
-  const disc = discs[d];
-  const t = d + 1;
-  const p = disc[1] + t; // after t , at this pos
-  const m = (disc[0] - p) % disc[0];
-  disc.push(m < 0 ? m + disc[0] : m);
+function syncTime(discs: Discs): number {
+  let t = 0;
+  let mult = 1;
+  let running = true;
+  const seen = new Set<number>();
+  while (running) {
+    const match = discs.every((d, i) => {
+      const [n, p] = d;
+      const positionAtDropTime = t + p + i + 1;
+      const m = positionAtDropTime % n;
+      if (m === 0 && !seen.has(n)) {
+        seen.add(n);
+        mult *= n;
+      }
+      return m === 0;
+    });
+    if (match) {
+      running = false;
+    } else {
+      t += mult;
+    }
+  }
+  return t;
 }
 
-let t = 0;
-let mult = 1;
-let running = true;
-const seen = new Set<number>();
-while (running) {
-  const match = discs.every((d) => {
-    const [n, p] = d;
-    const m = t % n;
-    if (m === p && !seen.has(n)) {
-      seen.add(n);
-      // mult *= n;
-    }
-    console.log(t, n, p, m);
-    return m === p;
-  });
-  if (match) {
-    running = false;
-  } else {
-    t += mult;
-  }
-}
-console.log(t);
-console.log(discs);
+test(5, syncTime(testDiscs));
+console.log("Part One", syncTime(inputDiscs));
+console.log("Part Two", syncTime(inputDiscs.concat([[11, 0]])));
