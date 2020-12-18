b10b029b807d63db3acb0bb00f691800078a4bbc
Tom Saunders <tom@tsaunders.net>
2020-12-18 20:05:53 +1100

    2020.18

--- advent16/11.ts
+++ advent16/11.ts
@@ -1,80 +1,133 @@
 #!/usr/bin/env ts-node
 import * as fs from "fs";
-import { test } from "./util";
-const input = fs.readFileSync("input10.txt", "utf8");
-const lines = input.split("\n");
-lines.sort();
+import { arrProd, arrSum, test } from "./util";
+const input = `The first floor contains a polonium generator, a thulium generator, a thulium-compatible microchip, a promethium generator, a ruthenium generator, a ruthenium-compatible microchip, a cobalt generator, and a cobalt-compatible microchip.
+The second floor contains a polonium-compatible microchip and a promethium-compatible microchip.
+The third floor contains nothing relevant.
+The fourth floor contains nothing relevant.`;
 
-const botMax = 210;
-let running = true;
+const testIn = `The first floor contains a hydrogen-compatible microchip and a lithium-compatible microchip.
+The second floor contains a hydrogen generator.
+The third floor contains a lithium generator.
+The fourth floor contains nothing relevant.`;
 
-const bots: Bot[] = [];
-const outputs: number[] = [];
+interface GameState {
+  e: number;
+  f: string[];
+}
 
-class Bot {
-  public chips: number[] = [];
-  constructor(
-    public id: number,
-    public lowType: string,
-    public lowTo: number,
-    public highType: string,
-    public highTo: number
-  ) {}
-  public get ready(): boolean {
-    return this.chips.length === 2;
+// polonium 1 thulium 2 promethium 3 ruthenium 4 cobalt 5
+const inputState: GameState = {
+  e: 0,
+  f: ["1G, 2G, 2M, 3G, 4G, 4M, 5G, 5M", "1M, 3M", "", ""],
+};
+
+// hydrogen 1 lithium 2
+const testState: GameState = {
+  e: 0,
+  f: ["1M,2M", "1G", "2G", ""],
+};
+
+class Game {
+  public elevator: number;
+  public map: Map<string, Device>;
+  public floors: Device[][] = [];
+  constructor(state: GameState) {
+    this.elevator = state.e;
+    this.map = new Map<string, Device>();
+    this.floors = state.f.map((devices: string): Device[] =>
+      devices.split(",").map((id) => Device.init(id.trim(), this))
+    );
   }
-  public run(): void {
-    this.chips.sort((a, b) => a - b);
-    const low = this.chips.shift() as number;
-    const high = this.chips.shift() as number;
-
-    if (low === 17 && high === 61) {
-      console.log("Part One", this.id);
+  toJSON(): GameState {
+    return {
+      e: this.elevator,
+      f: this.floors.map((devices: Device[]): string =>
+        devices.map((d) => d.id).join(",")
+      ),
+    };
+  }
+  get valid(): boolean {
+    return this.floors.every((floor: Device[]) => {
+      const containsGenerator = !!floor.find((d) => d instanceof Gen);
+      const containsUnshielded = !!floor.find(
+        (d) => d instanceof Chip && !d.isShielded
+      );
+      return !(containsGenerator && containsUnshielded);
+    });
+  }
+  get elevatorOptions(): number[] {
+    const options = [];
+    if (this.elevator !== 3) {
+      options.push(1);
     }
-    if (this.lowType === "bot") bots[this.lowTo].chips.push(low);
-    else outputs[this.lowTo] = low;
-    if (this.highType === "bot") bots[this.highTo].chips.push(high);
-    else outputs[this.highTo] = high;
+
+    if (this.elevator !== 0) {
+      options.push(-1);
+    }
+
+    return options;
   }
-  public static fromLine(line: string): Bot {
-    //bot 24 gives low to bot 129 and high to bot 192
-    const bits = line.split(" ");
-    return new Bot(
-      parseInt(bits[1], 10),
-      bits[5],
-      parseInt(bits[6], 10),
-      bits[10],
-      parseInt(bits[11], 10)
+
+  get score(): number {
+    return arrProd(
+      this.floors.map((devices: Device[]) =>
+        arrProd(devices.map((d) => 4 - d.floor))
+      )
+    );
+  }
+
+  get win(): boolean {
+    return (
+      this.valid &&
+      this.elevator === 3 &&
+      this.floors[3].length === this.map.size
     );
   }
 }
 
-for (let i = 0; i < botMax; i++) {
-  bots.push(new Bot(i, "", 0, "", 0));
-  outputs.push(0);
-}
-
-for (const line of lines) {
-  if (line.startsWith("bot")) {
-    const b = Bot.fromLine(line);
-    bots[b.id] = b;
-  } else {
-    const bits = line.split(" ");
-    const chip = parseInt(bits[1], 10);
-    const dest = parseInt(bits[5], 10);
-    bots[dest].chips.push(chip);
+class Device {
+  constructor(public id: string, protected game: Game) {
+    game.map.set(id, this);
   }
-}
-while (running) {
-  running = false;
-  for (const bot of bots) {
-    if (bot.ready) {
-      bot.run();
-      running = true;
+  public floor: number = 0;
+  public get code(): string {
+    return this.id[0];
+  }
+  static init(key: string, game: Game): Device {
+    if (key.endsWith("M")) {
+      return new Chip(key, game);
+    } else {
+      return new Gen(key, game);
     }
   }
 }
-console.log("Part Two", outputs[0] * outputs[1] * outputs[2]);
-// value 7 goes to bot 174
-// bot 24 gives low to bot 129 and high to bot 192
-// Based on your instructions, what is the number of the bot that is responsible for comparing value-61 microchips with value-17 microchips?
+class Chip extends Device {
+  public get gen(): Gen {
+    return this.game.map.get(this.code + "G") as Gen;
+  }
+  public get isShielded(): boolean {
+    const gen = this.gen;
+    return gen.floor === this.floor;
+  }
+}
+class Gen extends Device {
+  public get chip(): Gen {
+    return this.game.map.get(this.code + "M") as Gen;
+  }
+}
+
+const g = new Game(testState);
+console.log(g, JSON.stringify(g), g.valid, g.score);
+
+// goal - get everything to the fourth floor
+// elevator starts on the first floor and can carry at most two things
+// if a microchip is on the same level as a generator without being protected by its own, dead
+
+// for each move,
+// what gets off the elevator (everything)
+// what gets on the elevator (1-2 items)
+// does the elevator go up or down
+// is the game left in a valid state
+// goal alg - what is the distance for each component to the goal
+// then, what is the fewest steps
--- advent16/24.ts
+++ advent16/24.ts
@@ -14,24 +14,20 @@ testGrid.draw();
 const grid = Grid.fromLines(input);
 // grid.draw();
 
-function shortestRoute(grid: Grid): number {
+function shortestRoute(grid: Grid, returnToStart: boolean = false): number {
   const nodes = grid.cells.filter((c) => c.type !== WALL && c.type !== SPACE);
 
   const distances = new Map<string, number>();
-  // for (let a = 0; a < nodes.length; a++) {
-  //   for (let b = a + 1; b < nodes.length; b++) {
-  //     const x = nodes[a].type;
-  //     const y = nodes[b].type;
-  //     const d = grid.shortestPath(nodes[a], nodes[b]);
-  //     distances.set(`${x}-${y}`, d);
-  //     distances.set(`${y}-${x}`, d);
-  //   }
-  // }
-  console.log(
-    grid.shortestPath(grid.getCell(1, 1) as Cell, grid.getCell(1, 3) as Cell)
-  );
-
-  return 0;
+  for (let a = 0; a < nodes.length; a++) {
+    grid.distancesFrom(nodes[a]);
+    for (let b = a + 1; b < nodes.length; b++) {
+      const x = nodes[a].type;
+      const y = nodes[b].type;
+      const d = nodes[b].tentativeDist;
+      distances.set(`${x}-${y}`, d);
+      distances.set(`${y}-${x}`, d);
+    }
+  }
 
   const options = nodes.map((c) => c.type).filter((x) => x !== "0"); // exclude start
 
@@ -39,17 +35,19 @@ function shortestRoute(grid: Grid): number {
   permute(permutations, options, ["0"]);
   const distanceArr = permutations.map((steps: string[]) => {
     let d = 0;
+    if (returnToStart) {
+      steps.push("0");
+    }
     for (let i = 1; i < steps.length; i++) {
       const last = steps[i - 1];
       const step = steps[i];
       d += distances.get(`${last}-${step}`) as number;
     }
-    console.log(steps, d);
     return d;
   });
-  console.log(distances);
-  console.log(nodes[0]);
   return Math.min(...distanceArr);
 }
 
 test(shortestRoute(testGrid), 14);
+console.log("Part One", shortestRoute(grid));
+console.log("Part Two", shortestRoute(grid, true));
