c4a96ac8eef62fd8d17fbb6c68dd2a6bbe889266
Tom Saunders <tom@tsaunders.net>
2024-12-01 16:42:03 +1100

    24.1

--- advent24/1.ts
+++ advent24/1.ts
@@ -2,28 +2,53 @@
 import * as fs from "fs";
 import { arrSum } from "./util";
 const input = fs.readFileSync("input1.txt", "utf8");
-const test = ``;
+const test = `3   4
+4   3
+2   5
+1   3
+3   9
+3   3`;
 
-const test2 = ``;
+function parseInput(input: string): [number[], number[]] {
+  const lines = input.split("\n");
+  const left: number[] = [];
+  const right: number[] = [];
+  lines.forEach((line) => {
+    const bits = line.split(" ").filter(Boolean);
+    left.push(parseInt(bits[0]));
+    right.push(parseInt(bits[1]));
+  });
+  return [left, right];
+}
 
 function part1(input: string): number {
-  const lines = input.split("\n");
-  return 0;
+  const [leftCol, rightCol] = parseInput(input);
+  leftCol.sort((a, b) => a - b);
+  rightCol.sort((a, b) => a - b);
+
+  return leftCol.reduce(
+    (sum, left, i) => sum + Math.abs(rightCol[i] - left),
+    0
+  );
 }
 
 function part2(input: string): number {
-  const lines = input.split("\n");
-  return 0;
+  const [leftCol, rightCol] = parseInput(input);
+
+  return leftCol.reduce((sum, left) => {
+    const similarityCount = rightCol.filter((right) => right === left).length;
+    return sum + similarityCount * left;
+  }, 0);
 }
 
 const t = part1(test);
-if (t == 142) {
+if (t == 11) {
   console.log("part 1 answer", part1(input));
 } else {
   console.log("part 1 test fail", t);
 }
-const t2 = part2(test2);
-if (t2 == 281) {
+const t2 = part2(test);
+if (t2 == 31) {
   console.log("part 2 answer", part2(input));
 } else {
   console.log("part 2 test fail", t2);
