ee8b50b2a33045164266d4e7c7c0d0b59bd16ca0
Tom Saunders <tomsaunders@canva.com>
2025-12-01 15:16:17 +1100

    aoc25 setup

---
+++ advent25/util/cell.ts
@@ -0,0 +1,249 @@
+import { Grid } from "./grid";
+import { RED, RESET, WHITE, WALL, SPACE, GREEN, ON, OFF, Direction } from ".";
+
+export type CellCreator = (
+  grid: Grid,
+  x: number,
+  y: number,
+  z: number,
+  type: string,
+) => Cell;
+
+export class Cell {
+  public reference?: string;
+  constructor(
+    public grid: Grid,
+    public x: number,
+    public y: number,
+    public z: number,
+    public type: string,
+  ) {}
+
+  public visited = false;
+  public tentativeDist = 9999;
+  public guessDist = 9999;
+  public knownDistances: [Cell, number][] = [];
+  public cost: number = 1;
+
+  public north?: Cell;
+  public south?: Cell;
+  public east?: Cell;
+  public west?: Cell;
+  // NSEW which are open
+  public openNeighbours: Cell[] = [];
+  // NSEW array
+  public directNeighbours: Cell[] = [];
+  // diagonals
+  public diagonalNeighbours: Cell[] = [];
+  // NSEW + diagonals
+  public allNeighbours: Cell[] = [];
+
+  public init(doReset = true, createNeighbours = false): void {
+    this.north = this.getNorth(createNeighbours);
+    this.south = this.getSouth(createNeighbours);
+    this.east = this.getEast(createNeighbours);
+    this.west = this.getWest(createNeighbours);
+
+    this.directNeighbours = [
+      this.north,
+      this.south,
+      this.east,
+      this.west,
+    ].filter((c) => !!c) as Cell[];
+
+    this.diagonalNeighbours = [
+      this.getNorthWest(createNeighbours),
+      this.getNorthEast(createNeighbours),
+      this.getSouthWest(createNeighbours),
+      this.getSouthEast(createNeighbours),
+    ].filter((c) => !!c) as Cell[];
+
+    this.openNeighbours = this.directNeighbours.filter((c) => c.isSpace);
+    this.allNeighbours = this.diagonalNeighbours.concat(this.directNeighbours);
+
+    this.knownDistances = [];
+    if (doReset) {
+      this.reset();
+    }
+  }
+
+  public get hexNeighbours(): Cell[] {
+    return [
+      this.grid.getCell(this.x + 1, this.y - 1, this.z, true), //e
+      this.grid.getCell(this.x - 1, this.y + 1, this.z, true), //w
+      this.grid.getCell(this.x + 1, this.y, this.z - 1, true), //ne
+      this.grid.getCell(this.x, this.y + 1, this.z - 1, true), //nw
+      this.grid.getCell(this.x, this.y - 1, this.z + 1, true), //se
+      this.grid.getCell(this.x - 1, this.y, this.z + 1, true), //sw
+    ] as Cell[];
+  }
+
+  public getCubeNeighbours(createNeighbours = false): Cell[] {
+    return [
+      this.getNorth(createNeighbours),
+      this.getSouth(createNeighbours),
+      this.getEast(createNeighbours),
+      this.getWest(createNeighbours),
+      this.getUp(createNeighbours),
+      this.getDown(createNeighbours),
+    ].filter((c) => !!c) as Cell[];
+  }
+
+  public reset(): void {
+    this.visited = false;
+    this.tentativeDist = 9999;
+  }
+
+  public get coord(): string {
+    return `${this.x}:${this.y}:${this.z}`;
+  }
+
+  public get xy(): string {
+    return `${this.x}, ${this.y}`;
+  }
+
+  public get label(): string {
+    return `${this.code} @ ${this.coord}`;
+  }
+
+  public toString(): string {
+    return this.label;
+  }
+
+  public get kdInfo(): string {
+    const kd = this.knownDistances
+      .map((v: [Cell, number]) => `${v[1]} to ${v[0].coord}`)
+      .join("\n\t");
+    return `${this.label} knows distances = \n\t${kd}`;
+  }
+
+  public get unexplored(): Cell[] {
+    return this.openNeighbours.filter((n) => !n.visited);
+  }
+
+  public get code(): string {
+    if (this.knownDistances.length) {
+      return `${RED}X${RESET}`;
+    }
+    if (this.visited) {
+      return `${GREEN}${this.type}${RESET}`;
+    }
+    if (this.isSpace) {
+      return `${WHITE}${this.type}${RESET}`;
+    }
+    return this.type;
+  }
+
+  public get charCode(): number {
+    return this.type.charCodeAt(0);
+  }
+
+  public get isWall(): boolean {
+    return this.type === WALL;
+  }
+
+  public get isSpace(): boolean {
+    return this.type === SPACE;
+  }
+
+  public get onBorder(): boolean {
+    return (
+      this.x == this.grid.maxX ||
+      this.x == this.grid.minX ||
+      this.y == this.grid.minY ||
+      this.y == this.grid.maxY
+    );
+  }
+
+  public get isOn(): boolean {
+    return this.type === ON;
+  }
+
+  public get isOff(): boolean {
+    return this.type === OFF;
+  }
+
+  public toggle(): this {
+    this.type = this.type === ON ? OFF : ON;
+    return this;
+  }
+
+  public get isIntersection(): boolean {
+    return this.openNeighbours.length > 2 && this.isSpace;
+  }
+
+  public get int(): number {
+    return parseInt(this.type, 10);
+  }
+
+  public clone(): Cell {
+    return this.cloneToGrid(this.grid);
+  }
+
+  public cloneToGrid(grid: Grid): Cell {
+    return new Cell(grid, this.x, this.y, this.z, this.type);
+  }
+
+  public getCell(dx: number, dy: number): Cell | undefined {
+    return this.grid.getCell(this.x + dx, this.y + dy);
+  }
+
+  public getDirection(
+    direction: Direction,
+    createNeighbours = false,
+  ): Cell | undefined {
+    if (direction == "North" || direction === "n") {
+      return this.grid.getCell(this.x, this.y - 1, this.z, createNeighbours);
+    } else if (direction == "South" || direction === "s") {
+      return this.grid.getCell(this.x, this.y + 1, this.z, createNeighbours);
+    } else if (direction == "East" || direction === "e") {
+      return this.grid.getCell(this.x + 1, this.y, this.z, createNeighbours);
+    } else if (direction == "West" || direction === "w") {
+      return this.grid.getCell(this.x - 1, this.y, this.z, createNeighbours);
+    }
+  }
+
+  public getNorth(createNeighbours = false): Cell | undefined {
+    return this.grid.getCell(this.x, this.y - 1, this.z, createNeighbours);
+  }
+
+  public getSouth(createNeighbours = false): Cell | undefined {
+    return this.grid.getCell(this.x, this.y + 1, this.z, createNeighbours);
+  }
+
+  public getEast(createNeighbours = false): Cell | undefined {
+    return this.grid.getCell(this.x + 1, this.y, this.z, createNeighbours);
+  }
+
+  public getWest(createNeighbours = false): Cell | undefined {
+    return this.grid.getCell(this.x - 1, this.y, this.z, createNeighbours);
+  }
+
+  public getNorthWest(createNeighbours = false): Cell | undefined {
+    return this.grid.getCell(this.x - 1, this.y - 1, this.z, createNeighbours);
+  }
+
+  public getNorthEast(createNeighbours = false): Cell | undefined {
+    return this.grid.getCell(this.x + 1, this.y - 1, this.z, createNeighbours);
+  }
+
+  public getSouthWest(createNeighbours = false): Cell | undefined {
+    return this.grid.getCell(this.x - 1, this.y + 1, this.z, createNeighbours);
+  }
+
+  public getSouthEast(createNeighbours = false): Cell | undefined {
+    return this.grid.getCell(this.x + 1, this.y + 1, this.z, createNeighbours);
+  }
+
+  public getUp(createNeighbours = false): Cell | undefined {
+    return this.grid.getCell(this.x, this.y, this.z + 1, createNeighbours);
+  }
+
+  public getDown(createNeighbours = false): Cell | undefined {
+    return this.grid.getCell(this.x, this.y, this.z - 1, createNeighbours);
+  }
+
+  public getDistance(otherCell: Cell): number {
+    return Math.abs(this.x - otherCell.x) + Math.abs(this.y - otherCell.y);
+  }
+}
---
+++ advent25/util/grid.ts
@@ -0,0 +1,262 @@
+import { Cell, CellCreator } from "./cell";
+import { WHITE } from ".";
+
+export class Grid {
+  public lines: string[] = [];
+  public lookup: Map<string, Cell>;
+  public minY: number = 0;
+  public minX: number = 0;
+  public maxY: number = 0;
+  public maxX: number = 0;
+  public minZ: number = 0;
+  public maxZ: number = 0;
+  public _cells: Cell[][] = [];
+  constructor() {
+    this.lookup = new Map<string, Cell>();
+  }
+
+  public get cells(): Cell[] {
+    return Array.from(this.lookup.values());
+  }
+
+  public get hash(): string {
+    return this.cells.map((c) => c.type).join("");
+  }
+
+  public reset(): void {
+    this.cells.forEach((c) => c.reset());
+  }
+
+  public createCell(
+    x: number,
+    y: number,
+    z: number,
+    type: string,
+    cellCreator?: CellCreator
+  ): Cell {
+    const c = cellCreator
+      ? cellCreator(this, x, y, z, type)
+      : new Cell(this, x, y, z, type);
+
+    return this.addCell(c);
+  }
+
+  public addCell(c: Cell): Cell {
+    this.lookup.set(c.coord, c);
+    this.minY = Math.min(this.minY, c.y);
+    this.minX = Math.min(this.minX, c.x);
+    this.maxY = Math.max(this.maxY, c.y);
+    this.maxX = Math.max(this.maxX, c.x);
+    this.minZ = Math.min(this.minZ, c.z);
+    this.maxZ = Math.max(this.maxZ, c.z);
+
+    return c;
+  }
+
+  public getCell(x: number, y: number, z: number = 0, createIfNot = false) {
+    const c = this.getByCoord(`${x}:${y}:${z}`);
+    if (!c && createIfNot) {
+      return this.createCell(x, y, z, ".");
+    }
+    return c;
+  }
+
+  public getByCoord(coord: string) {
+    return this.lookup.get(coord);
+  }
+
+  public init(): this {
+    this.cells.forEach((c) => c.init());
+    return this;
+  }
+
+  public draw(z: number = 0, drawCoords: boolean = true): void {
+    const YELLOW: string = "\x1b[33m";
+    const RESET: string = "\x1b[0m";
+    const yPad = Math.max(this.maxY, Math.abs(this.minY)).toString().length;
+
+    if (drawCoords) {
+      // draw two rows for the x headers
+      // every 10, print the leading digits on the first row
+      // and print the last digit on the second
+      // 0         1         2
+      // 0123456789012345678901234
+      const spacer = "".padStart(yPad + 1, " ");
+      let xRow = `${YELLOW}${spacer}`;
+      for (let x = this.minX; x <= this.maxX; x++) {
+        // when the x coordinate goes into triple digits,
+        // the header will be in double digits,
+        // so don't add a spacer for the next character to preserve alignment
+        const spacerX = x > 99 && x % 10 === 1 ? "" : " ";
+        xRow += x % 10 === 0 ? Math.round(Math.abs(x) / 10) : spacerX;
+      }
+      xRow = `${xRow}${RESET}`;
+      console.log(xRow);
+      xRow = `${YELLOW}${spacer}`;
+      for (let x = this.minX; x <= this.maxX; x++) {
+        xRow += Math.abs(x) % 10;
+      }
+      xRow = `${xRow}${RESET}`;
+      console.log(xRow);
+    }
+
+    for (let y = this.minY; y <= this.maxY; y++) {
+      // if drawing coordinates, pad the y value according to the maximum number size
+      // before then drawing the rest of the row
+      const yStr = Math.abs(y).toString();
+      const yPos = yStr.padStart(yPad, "0");
+      let line = drawCoords ? `${YELLOW}${yPos}${RESET} ` : "";
+
+      for (let x = this.minX; x <= this.maxX; x++) {
+        const c = `${x}:${y}:${z}`;
+        const cell = this.lookup.get(c);
+        line += cell ? cell.code : " ";
+      }
+      console.log(line);
+    }
+  }
+
+  public drawAll(drawCoords: boolean = true): void {
+    for (let i = this.minZ; i <= this.maxZ; i++) {
+      console.log("z=", i);
+      this.draw(i, drawCoords);
+    }
+  }
+
+  public breadthFirst(
+    from: Cell,
+    getOpenNeighbours?: (c: Cell) => Cell[]
+  ): void {
+    if (!getOpenNeighbours) {
+      getOpenNeighbours = (c: Cell) => c.openNeighbours;
+    }
+    let unvisitedSet: Cell[] = [from];
+    from.tentativeDist = 0;
+    while (unvisitedSet.length) {
+      unvisitedSet.sort((a, b) => b.tentativeDist - a.tentativeDist);
+      let c = unvisitedSet.pop() as Cell;
+      c.visited = true;
+      for (const n of getOpenNeighbours(c)) {
+        const d = c.tentativeDist + n.cost;
+        if (d < n.tentativeDist) {
+          n.tentativeDist = d;
+          unvisitedSet.push(n);
+        }
+      }
+    }
+  }
+
+  public shortestPath(
+    from: Cell,
+    to: Cell,
+    getOpenNeighbours?: (c: Cell) => Cell[]
+  ): number {
+    if (!getOpenNeighbours) {
+      getOpenNeighbours = (c: Cell) => c.openNeighbours;
+    }
+
+    let unvisitedSet: Cell[] = this.cells.slice(0);
+    from.tentativeDist = 0;
+    let c: Cell = from;
+
+    while (!to.visited && unvisitedSet.length) {
+      for (const n of getOpenNeighbours(c)) {
+        const d = c.tentativeDist + n.cost;
+        n.tentativeDist = Math.min(d, n.tentativeDist);
+      }
+      c.visited = true;
+      unvisitedSet.sort((a, b) => b.tentativeDist - a.tentativeDist);
+      c = unvisitedSet.pop() as Cell;
+    }
+    return to.tentativeDist;
+  }
+
+  public aStar(from: Cell, to: Cell): Cell[] {
+    let openSet: Cell[] = [from];
+    // tentative dist is the shortest known distance from 'from' to a cell. 0 at start.
+    from.tentativeDist = 0;
+
+    // use manhattan distance as heuristic
+    const h = (c: Cell) => Math.abs(to.x - c.x) + Math.abs(to.y - c.y) + c.cost;
+    // guess dist is the estimated cost of going start to end via this. The more accurate the estimate, the faster the path will be found
+    from.guessDist = h(from);
+
+    // For node n, cameFrom[n] is the node immediately preceding it on the cheapest path from start
+    // to n currently known.
+    const cameFrom: Map<Cell, Cell> = new Map<Cell, Cell>();
+
+    const reconstructPath = (start: Cell): Cell[] => {
+      const path = [start];
+      let c = start;
+      while (c !== from) {
+        c = cameFrom.get(c) as Cell;
+        path.unshift(c);
+      }
+      return path;
+    };
+
+    while (openSet.length) {
+      const current = openSet.pop() as Cell;
+      if (current === to) {
+        return reconstructPath(current);
+      }
+
+      for (const n of current.directNeighbours) {
+        const tentativeGScore = current.tentativeDist + n.int;
+        const gScore = n.tentativeDist || 9999;
+        if (tentativeGScore < gScore) {
+          cameFrom.set(n, current);
+          n.tentativeDist = tentativeGScore;
+          n.guessDist = tentativeGScore + h(n);
+          if (!openSet.includes(n)) {
+            openSet.push(n);
+          }
+        }
+      }
+      openSet.sort((a, b) => b.guessDist - a.guessDist);
+    }
+
+    // Open set is empty but goal was never reached
+    return [];
+  }
+
+  public static fromLines(
+    input: string | string[],
+    cellCreator?: CellCreator
+  ): Grid {
+    const lines = Array.isArray(input) ? input : input.split("\n");
+
+    const g = new Grid();
+    for (let y = 0; y < lines.length; y++) {
+      for (let x = 0; x < lines[y].length; x++) {
+        g.createCell(x, y, 0, lines[y][x], cellCreator);
+      }
+    }
+    return g.init();
+  }
+
+  public clone(): Grid {
+    const g = new Grid();
+    Array.from(this.lookup.values()).forEach((c) => {
+      g.addCell(c.cloneToGrid(g));
+    });
+    return g.init();
+  }
+
+  public makeAllCells(): void {
+    for (let x = this.minX; x <= this.maxX; x++) {
+      for (let y = this.minY; y <= this.maxY; y++) {
+        this.getCell(x, y, 0, true);
+      }
+    }
+    for (let x = this.minX; x <= this.maxX; x++) {
+      for (let y = this.minY; y <= this.maxY; y++) {
+        this.getCell(x, y)?.init();
+      }
+    }
+  }
+
+  public findByType(type: string): Cell | undefined {
+    return this.cells.find((c) => c.type === type);
+  }
+}
