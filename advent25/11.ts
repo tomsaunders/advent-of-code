3b481a133ed1ccff33f43a60c0222e5a24ebfa0c
Tom Saunders <tomsaunders@canva.com>
2025-12-24 10:42:16 +1100

    wip 11

--- advent25/11.ts
+++ advent25/11.ts
@@ -42,6 +42,7 @@ hhh: out`;
 
 class Device {
   public outputs: Device[] = [];
+  public visitableDevices: Set<string> = new Set<string>();
   constructor(public name: string) {}
   public toString() {
     return this.name;
@@ -92,12 +93,20 @@ function parseInput(input: string): Record<string, Device> {
     );
   }
 
-  console.log(
-    "cleaned up input",
-    input.split("\n").length,
-    "to",
-    Object.values(devices).length,
-  );
+  allDevices.forEach((device) => {
+    // for each device, work out all of the places it can go by tracing to the end.
+    const queue: Device[] = device.outputs.slice(0);
+    function add(d: Device) {
+      if (!device.visitableDevices.has(d.name)) {
+        device.visitableDevices.add(d.name);
+        queue.push(...d.outputs);
+      }
+    }
+    while (queue.length) {
+      const d = queue.shift()!;
+      add(d);
+    }
+  });
 
   return devices;
 }
@@ -130,26 +139,41 @@ function part2(input: string): number {
   const devices = parseInput(input);
 
   function costFromTo(start: Device, goal: Device, abort: Device[]): number {
-    let pathCount = 0;
-
-    const paths: Device[][] = [[start]];
-    while (paths.length) {
-      const path = paths.shift() as Device[];
-      const [at] = path;
-      if (at === goal) {
-        console.log("found path", path.join(", "));
-        pathCount++;
-        continue;
-      } else if (abort.includes(at)) {
-        continue;
+    const reverseMap: Record<string, Device[]> = {};
+    // create a map of inputs for each output
+    Object.values(devices).forEach((device) => {
+      if (!start.visitableDevices.has(device.name)) {
+        // this device is not visitable from our start point so we can ignore.
+        return;
       }
-      at.outputs.forEach((out) => {
-        const outPath = [out, ...path.slice(0)];
-        paths.push(outPath);
+      device.outputs.forEach((outD) => {
+        if (!start.visitableDevices.has(outD.name)) {
+          // this device is not visitable from our start point so we can ignore.
+          return;
+        }
+        if (!reverseMap[outD.name]) reverseMap[outD.name] = [];
+        reverseMap[outD.name].push(device);
       });
-    }
+    });
 
-    return pathCount;
+    console.log(reverseMap);
+    return 0;
+
+    // count the ways you can get from
+    // const outToWays: Record<string, number> = { [start.name]: 1 };
+    // while (!outToWays[goal.name]) {
+    //   if (!Object.keys(reverseMap).length) return 0;
+    //   for (const out in reverseMap) {
+    //     const ways = reverseMap[out]
+    //       .map((d) => outToWays[d.name])
+    //       .reduce((a, b) => a + b, 0);
+    //     if (!isNaN(ways)) {
+    //       delete reverseMap[out];
+    //     }
+    //     outToWays[out] = ways;
+    //   }
+    // }
+    // return outToWays[goal.name];
   }
 
   const svr = devices["svr"];
@@ -175,7 +199,61 @@ if (t == 5) {
 }
 const t2 = part2(test2);
 if (t2 == 2) {
+  // should be 473741288064360
   console.log("part 2 answer", part2(input));
 } else {
   console.log("part 2 test fail", t2);
 }
+
+/**
+ * const input = require(`fs`).readFileSync(`input11.txt`).toString`utf8`
+   .trim()
+   .split(/\r?\n/)
+   .map((line) => line.split(": "))
+   .map(([ins, outs]) => [ins, outs.split(" ")]);
+ const inToOut = Object.fromEntries(input);
+ 
+ console.log(solve("you", "out"));
+ console.log(
+   solve("svr", "dac") * solve("dac", "fft") * solve("fft", "out") +
+     solve("svr", "fft") * solve("fft", "dac") * solve("dac", "out"),
+ );
+ 
+ function solve(start, end) {
+   const visitableNodes = new Set([start]);
+   let size = 0;
+   while (visitableNodes.size > size) {
+     size = visitableNodes.size;
+     [...visitableNodes]
+       .flatMap((n) => inToOut[n])
+       .filter((x) => x !== undefined)
+       .forEach(visitableNodes.add, visitableNodes);
+   }
+ 
+   const outToIn = {};
+   for (const [inPath, outs] of input) {
+     if (!visitableNodes.has(inPath)) continue;
+     for (const out of outs) {
+       if (!visitableNodes.has(out)) continue;
+       if (!outToIn[out]) outToIn[out] = [];
+       outToIn[out].push(inPath);
+     }
+   }
+ 
+   const outToWays = { [start]: 1 };
+   while (!outToWays[end]) {
+     if (!Object.keys(outToIn).length) return 0;
+     for (const out in outToIn) {
+       const ways = outToIn[out]
+         .map((l) => outToWays[l])
+         .reduce((a, b) => a + b, 0);
+       if (!isNaN(ways)) {
+         delete outToIn[out];
+       }
+       outToWays[out] = ways;
+     }
+   }
+   return outToWays[end];
+ }
+ 
+ */
